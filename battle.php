f8c4979d11eb9240c2a39ea893a6ea211a15beae
Leanna Pelham <leanna.pelham@knplabs.com>
2015-01-06 11:16:12 -0500

    more base stuff

--- battle.php
+++ battle.php
@@ -31,7 +31,20 @@ $outcome = battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
 
 <html>
     <head>
+        <meta charset="utf-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <title>OO Battleships</title>
 
+           <!-- Bootstrap -->
+           <link href="css/bootstrap.min.css" rel="stylesheet">
+
+           <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+           <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+           <!--[if lt IE 9]>
+             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+           <![endif]-->
     </head>
     <body>
         <div>
@@ -65,5 +78,11 @@ $outcome = battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
         </div>
 
         <a href="/index.php">Battle again</a>
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        
     </body>
 </html>
