5168d7eb954fbaaeb1f6534bfe171ad62b7bbdbc
Victor <bocharsky.bw@gmail.com>
2015-09-26 22:25:26 +0300

    Add code activities for ch3 in ep3

---
+++ knpu/episode3/Challenges/ProtectedVisibility/GetInheritedPropertyCoding.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Challenges\ProtectedVisibility;
+
+use KnpU\ActivityRunner\Activity\CodingChallenge\CodingContext;
+use KnpU\ActivityRunner\Activity\CodingChallenge\CorrectAnswer;
+use KnpU\ActivityRunner\Activity\CodingChallengeInterface;
+use KnpU\ActivityRunner\Activity\CodingChallenge\CodingExecutionResult;
+use KnpU\ActivityRunner\Activity\CodingChallenge\FileBuilder;
+use KnpU\ActivityRunner\Activity\Exception\GradingException;
+
+class GetInheritedPropertyCoding implements CodingChallengeInterface
+{
+    /**
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return <<<EOF
+The construction of the `DeathStarII` continues, but we need access
+to the `planetarySuperLaserRange` property from the original, because
+we're going to make it fire twice as far! Fix the `DeathStar` class
+so that the new `getSpecs()` method works:
+EOF;
+    }
+
+    public function getFileBuilder()
+    {
+        $fileBuilder = new FileBuilder();
+        $fileBuilder
+            ->addFileContents('index.php', <<<EOF
+<?php
+
+require 'DeathStar.php';
+require 'DeathStarII.php';
+
+\$deathStar = new DeathStarII();
+
+?>
+
+<h2>New DeathStar Specs</h2>
+<table>
+    <?php foreach (\$deathStar->getSpecs() as \$key => \$val): ?>
+    <tr>
+        <th><?php echo \$key; ?></th>
+        <td><?php echo \$val; ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+EOF
+            )
+            ->addFileContents('DeathStarII.php', <<<EOF
+<?php
+
+class DeathStarII extends DeathStar
+{
+    public function getSpecs()
+    {
+        return array(
+            'name' => 'DeathStarII',
+            'laser_range' => \$this->planetarySuperLaserRange * 2,
+        );
+    }
+}
+EOF
+            )
+            ->addFileContents('DeathStar.php', <<<EOF
+<?php
+
+class DeathStar
+{
+    private \$planetarySuperLaserRange = 2000000;
+}
+EOF
+            )
+            ->setEntryPointFilename('index.php')
+        ;
+
+        return $fileBuilder;
+    }
+
+    public function getExecutionMode()
+    {
+        return self::EXECUTION_MODE_PHP_NORMAL;
+    }
+
+    public function setupContext(CodingContext $context)
+    {
+    }
+
+    public function grade(CodingExecutionResult $result)
+    {
+        $class = new \ReflectionClass('\DeathStar');
+        try {
+            assertTrue(
+                $class->hasProperty('planetarySuperLaserRange'),
+                'The `planetarySuperLaserRange` property does not exist in `DeathStar` class.'
+            );
+            $property = $class->getProperty('planetarySuperLaserRange');
+            assertTrue(
+                $property->isProtected(),
+                'The `planetarySuperLaserRange` property should has protected visibility.'
+            );
+        } catch (\Exception $e) {
+            throw new GradingException($e->getMessage());
+        }
+    }
+
+    public function configureCorrectAnswer(CorrectAnswer $correctAnswer)
+    {
+        $correctAnswer
+            ->setFileContents('DeathStar.php', <<<EOF
+<?php
+
+class DeathStar
+{
+    protected \$planetarySuperLaserRange = 2000000;
+}
+EOF
+            )
+        ;
+    }
+}
---
+++ knpu/episode3/Challenges/ProtectedVisibility/PropertyVisibilityMC.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Challenges\ProtectedVisibility;
+
+use KnpU\ActivityRunner\Activity\MultipleChoice\AnswerBuilder;
+use KnpU\ActivityRunner\Activity\MultipleChoiceChallengeInterface;
+
+class PropertyVisibilityMC implements MultipleChoiceChallengeInterface
+{
+    /**
+     * @return string
+     */
+    public function getQuestion()
+    {
+        return <<<EOF
+Check out the following code:
+
+```php
+// Ship.php
+
+class Ship
+{
+    public \$name;
+
+    protected \$weaponPower;
+
+    private \$defense;
+
+    public function getDefense()
+    {
+        return \$this->defense;
+    }
+}
+```
+
+```php
+// JediShip.php
+
+class JediShip
+{
+    public function getWeaponPower()
+    {
+        return \$this->weaponPower;
+    }
+}
+```
+
+```php
+// index.php
+
+\$jediShip = new JediShip();
+\$jediShip->name;
+\$jediShip->weaponPower;
+```
+
+Which of the above code will give us an error?
+EOF;
+    }
+
+    /**
+     * @param AnswerBuilder $builder
+     */
+    public function configureAnswers(AnswerBuilder $builder)
+    {
+        $builder
+            ->addAnswer('`$jediShip->weaponPower` in `index.php`', true)
+            ->addAnswer('`return $this->defense` in `Ship.php`')
+            ->addAnswer('`return $this->weaponPower` in `RebelShip.php`')
+            ->addAnswer('None of the above is bad code - we\'re awesome!')
+        ;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getExplanation()
+    {
+        return <<<EOF
+Since the `\$defense` property is `private`, we can only access it from
+within the `Ship` class. But that's exactly what we're doing in (B),
+so that's fine.
+
+The `\$weaponPower` property is `protected`. That means we can access it
+only from inside `Ship`, `JediShip` or any other sub-classes. That's why
+(C) is valid. But in (A), we're accessing `weaponPower` from `index.php`.
+Accessing a property or method from outside of the class is only allowed
+if it is public. This is a bad code.
+EOF;
+    }
+}
