fd0a06e4b67a0bc5b3ef8e13aec0c2de11e24104
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:25 -0400

    Inheritance: Copy getNameAndSpecs

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -19,4 +19,25 @@ class RebelShip extends Ship
     {
         return true;
     }
+
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s (Rebel)',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s (Rebel)',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+    }
 }
