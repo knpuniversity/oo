152c42d59298b66f840073d84ba78fa7fa52b67a
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:29 -0400

    Abstract: Fill in abstract methods

--- lib/Model/BrokenShip.php
+++ lib/Model/BrokenShip.php
@@ -2,5 +2,18 @@
 
 class BrokenShip extends AbstractShip
 {
+    public function getJediFactor()
+    {
+        return 0;
+    }
 
+    public function getType()
+    {
+        return 'Broken';
+    }
+
+    public function isFunctional()
+    {
+        return false;
+    }
 }
