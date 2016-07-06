0ff3fbed44df5a014b0c025632b8773c91d8ff02
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:16 -0400

    Fixing battle.php: Removing array type-hint

--- battle.php
+++ battle.php
@@ -26,8 +26,6 @@ if ($ship1Quantity <= 0 || $ship2Quantity <= 0) {
 $ship1 = $ships[$ship1Name];
 $ship2 = $ships[$ship2Name];
 
-var_dump($ship1, $ship2);die;
-
 $outcome = battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
 ?>
 
--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@ function get_ships()
  *
  * @return array With keys winning_ship, losing_ship & used_jedi_powers
  */
-function battle(array $ship1, $ship1Quantity, array $ship2, $ship2Quantity)
+function battle($ship1, $ship1Quantity, $ship2, $ship2Quantity)
 {
     $ship1Health = $ship1['strength'] * $ship1Quantity;
     $ship2Health = $ship2['strength'] * $ship2Quantity;
