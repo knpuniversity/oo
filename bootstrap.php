3be36a52641f01be73ea1fe3bfa1be8749ea722b
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:32 -0400

    Autoload: Remove require statement

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,6 @@ require_once __DIR__.'/lib/Model/AbstractShip.php';
 require_once __DIR__.'/lib/Model/Ship.php';
 require_once __DIR__.'/lib/Model/RebelShip.php';
 require_once __DIR__.'/lib/Model/BrokenShip.php';
-require_once __DIR__.'/lib/Service/BattleManager.php';
 require_once __DIR__.'/lib/Service/ShipStorageInterface.php';
 require_once __DIR__.'/lib/Service/PdoShipStorage.php';
 require_once __DIR__.'/lib/Service/JsonFileShipStorage.php';
