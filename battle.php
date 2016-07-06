d40799158733e95c4d3751369a728369828785dc
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:36 -0400

    Magic Methods: Try to access private property

--- battle.php
+++ battle.php
@@ -95,7 +95,7 @@ $battleResult = $battleManager->battle($ship1, $ship1Quantity, $ship2, $ship2Qua
                 <h3>Remaining Strength</h3>
                 <dl class="dl-horizontal">
                     <dt><?php echo $ship1->getName(); ?></dt>
-                    <dd><?php echo $ship1->getStrength(); ?></dd>
+                    <dd><?php echo $ship1->strength; ?></dd>
                     <dt><?php echo $ship2->getName(); ?></dt>
                     <dd><?php echo $ship2->getStrength(); ?></dd>
                 </dl>
