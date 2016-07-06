282d2618f2f0e0ceea8d880fb9741da099433716
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:21 -0400

    Database: Hydrate the objects!

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -7,32 +7,15 @@ class ShipLoader
         $ships = array();
 
         $shipsData = $this->queryForShips();
-        var_dump($shipsData);die;
 
-        $ship = new Ship('Jedi Starfighter');
-        //$ship->setName('Jedi Starfighter');
-        $ship->setWeaponPower(5);
-        $ship->setJediFactor(15);
-        $ship->setStrength(30);
-        $ships['starfighter'] = $ship;
+        foreach ($shipsData as $shipData) {
+            $ship = new Ship($shipData['name']);
+            $ship->setWeaponPower($shipData['weapon_power']);
+            $ship->setJediFactor($shipData['jedi_factor']);
+            $ship->setStrength($shipData['strength']);
 
-        $ship2 = new Ship('CloakShape Fighter');
-        $ship2->setWeaponPower(2);
-        $ship2->setJediFactor(2);
-        $ship2->setStrength(70);
-        $ships['cloakshape_fighter'] = $ship2;
-
-        $ship3 = new Ship('Super Star Destroyer');
-        $ship3->setWeaponPower(70);
-        $ship3->setJediFactor(0);
-        $ship3->setStrength(500);
-        $ships['super_star_destroyer'] = $ship3;
-
-        $ship4 = new Ship('RZ-1 A-wing interceptor');
-        $ship4->setWeaponPower(4);
-        $ship4->setJediFactor(4);
-        $ship4->setStrength(50);
-        $ships['rz1_a_wing_interceptor'] = $ship4;
+            $ships[] = $ship;
+        }
 
         return $ships;
     }
