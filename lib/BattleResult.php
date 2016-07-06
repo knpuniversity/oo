7fa454fc65bbb0aa644d4d875dc88a60299474a1
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:19 -0400

    BattleResult: PHPDoc + getters

--- lib/BattleResult.php
+++ lib/BattleResult.php
@@ -6,10 +6,39 @@ class BattleResult
     private $winningShip;
     private $losingShip;
 
-    public function __construct($usedJediPowers, $winningShip, $losingShip)
+    /**
+     * @param Ship $winningShip
+     * @param Ship $losingShip
+     * @param boolean $usedJediPowers
+     */
+    public function __construct($usedJediPowers, Ship $winningShip, Ship $losingShip)
     {
         $this->usedJediPowers = $usedJediPowers;
         $this->winningShip = $winningShip;
         $this->losingShip = $losingShip;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isUsedJediPowers()
+    {
+        return $this->usedJediPowers;
+    }
+
+    /**
+     * @return Ship
+     */
+    public function getWinningShip()
+    {
+        return $this->winningShip;
+    }
+
+    /**
+     * @return Ship
+     */
+    public function getLosingShip()
+    {
+        return $this->losingShip;
     }
 }
