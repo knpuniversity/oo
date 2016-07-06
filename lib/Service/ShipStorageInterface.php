a5bc0603d4c8fe89e2539ba5e7fe426dc9b3e621
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:30 -0400

    Interface: Document it

--- lib/Service/ShipStorageInterface.php
+++ lib/Service/ShipStorageInterface.php
@@ -2,7 +2,23 @@
 
 interface ShipStorageInterface
 {
+    /**
+     * Returns an array of ship arrays, each with the following keys:
+     *  * id
+     *  * name
+     *  * weapon_power
+     *  * strength
+     *  * team
+     *
+     * @return array
+     */
     public function fetchAllShipsData();
 
+    /**
+     * Returns the single ship array for this id (see fetchAllShipsData)
+     *
+     * @param integer $id
+     * @return array
+     */
     public function fetchSingleShipData($id);
 }
