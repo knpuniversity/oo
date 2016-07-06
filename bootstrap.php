a599998d60d3f62400d45c236376a07e0d5df1a4
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:24 -0400

    Inheritance: Create RebelShip

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,7 @@ $configuration = array(
 
 require_once __DIR__.'/lib/Service/Container.php';
 require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Model/RebelShip.php';
 require_once __DIR__.'/lib/Service/BattleManager.php';
 require_once __DIR__.'/lib/Service/ShipLoader.php';
 require_once __DIR__.'/lib/Model/BattleResult.php';
