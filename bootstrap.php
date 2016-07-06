561951712fc41a69a5f75530d95aba4ac659bedd
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:33 -0400

    Autoload: Use composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,6 @@
 <?php
 
-spl_autoload_register(function($className) {
-    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
-
-    if (file_exists($path)) {
-        require $path;
-    }
-
-    // we don't support this class!
-});
+require __DIR__.'/vendor/autoload.php';
 
 $configuration = array(
     'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
