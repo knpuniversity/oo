60221debcb9528002792a8bafb4f53e3324166de
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:20 -0400

    Database: Make query in ShipLoader

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,6 +4,13 @@ class ShipLoader
 {
     public function getShips()
     {
+        $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('SELECT * FROM ship');
+        $statement->execute();
+        $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($shipsArray);die;
+
         $ships = array();
 
         $ship = new Ship('Jedi Starfighter');
