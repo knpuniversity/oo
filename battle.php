ccc72ac3bfcd7e1fe536634c396da6c783dae10e
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:18 -0400

    BattleManager: Basic creation and usage

--- battle.php
+++ battle.php
@@ -26,7 +26,8 @@ if ($ship1Quantity <= 0 || $ship2Quantity <= 0) {
 $ship1 = $ships[$ship1Name];
 $ship2 = $ships[$ship2Name];
 
-$outcome = battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
+$battleManager = new BattleManager();
+$outcome = $battleManager->battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
 ?>
 
 <html>
---
+++ lib/BattleManager.php
@@ -0,0 +1,58 @@
+<?php
+
+class BattleManager
+{
+    /**
+     * Our complex fighting algorithm!
+     *
+     * @return array With keys winning_ship, losing_ship & used_jedi_powers
+     */
+    public function battle(Ship $ship1, $ship1Quantity, Ship $ship2, $ship2Quantity)
+    {
+        $ship1Health = $ship1->getStrength() * $ship1Quantity;
+        $ship2Health = $ship2->getStrength() * $ship2Quantity;
+
+        $ship1UsedJediPowers = false;
+        $ship2UsedJediPowers = false;
+        while ($ship1Health > 0 && $ship2Health > 0) {
+            // first, see if we have a rare Jedi hero event!
+            if (didJediDestroyShipUsingTheForce($ship1)) {
+                $ship2Health = 0;
+                $ship1UsedJediPowers = true;
+
+                break;
+            }
+            if (didJediDestroyShipUsingTheForce($ship2)) {
+                $ship1Health = 0;
+                $ship2UsedJediPowers = true;
+
+                break;
+            }
+
+            // now battle them normally
+            $ship1Health = $ship1Health - ($ship2->getWeaponPower() * $ship2Quantity);
+            $ship2Health = $ship2Health - ($ship1->getWeaponPower() * $ship1Quantity);
+        }
+
+        if ($ship1Health <= 0 && $ship2Health <= 0) {
+            // they destroyed each other
+            $winningShip = null;
+            $losingShip = null;
+            $usedJediPowers = $ship1UsedJediPowers || $ship2UsedJediPowers;
+        } elseif ($ship1Health <= 0) {
+            $winningShip = $ship2;
+            $losingShip = $ship1;
+            $usedJediPowers = $ship2UsedJediPowers;
+        } else {
+            $winningShip = $ship1;
+            $losingShip = $ship2;
+            $usedJediPowers = $ship1UsedJediPowers;
+        }
+
+        return array(
+            'winning_ship' => $winningShip,
+            'losing_ship' => $losingShip,
+            'used_jedi_powers' => $usedJediPowers,
+        );
+    }
+}
