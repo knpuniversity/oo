a2754313b7d658eaac69ce7b884bfac2f563f937
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:14 -0400

    Using Objects: Use getName() method

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if (isset($_GET['error'])) {
                 <tbody>
                     <?php foreach ($ships as $ship): ?>
                         <tr>
-                            <td><?php echo $ship->name; ?></td>
+                            <td><?php echo $ship->getName(); ?></td>
                             <td><?php echo $ship->weaponPower; ?></td>
                             <td><?php echo $ship->jediFactor; ?></td>
                             <td><?php echo $ship->strength; ?></td>
