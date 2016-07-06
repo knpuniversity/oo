cc4d9720ce63421cf301bf3559e9d7c77730a296
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:13 -0400

    Organization: Moving Ship class to Ship.php

--- play.php
+++ play.php
@@ -1,52 +1,6 @@
 <?php
 
-class Ship
-{
-    public $name;
-
-    public $weaponPower = 0;
-
-    public $jediFactor = 0;
-
-    public $strength = 0;
-
-    public function sayHello()
-    {
-        echo 'Hello!';
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getNameAndSpecs($useShortFormat)
-    {
-        if ($useShortFormat) {
-            return sprintf(
-                '%s: %s/%s/%s',
-                $this->name,
-                $this->weaponPower,
-                $this->jediFactor,
-                $this->strength
-            );
-        } else {
-            return sprintf(
-                '%s: w:%s, j:%s, s:%s',
-                $this->name,
-                $this->weaponPower,
-                $this->jediFactor,
-                $this->strength
-            );
-        }
-    }
-
-    public function doesGivenShipHaveMoreStrength($givenShip)
-    {
-        return $givenShip->strength > $this->strength;
-    }
-
-}
+require_once __DIR__.'/lib/Ship.php';
 
 /**
  * @param Ship $someShip
