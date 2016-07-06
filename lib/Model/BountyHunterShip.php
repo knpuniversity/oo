db6ad39da2a2d1c516beddbdf157632de85409a3
Ryan Weaver <ryan@thatsquality.com>
2016-07-06 13:48:38 -0400

    Traits: Add BountyHunterShip empty

---
+++ lib/Model/BountyHunterShip.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Model;
+
+class BountyHunterShip extends AbstractShip
+{
+    public function getJediFactor()
+    {
+        // TODO: Implement getJediFactor() method.
+    }
+
+    public function getType()
+    {
+        // TODO: Implement getType() method.
+    }
+
+    public function isFunctional()
+    {
+        // TODO: Implement isFunctional() method.
+    }
+}
